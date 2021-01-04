12b7c6d1783d37a91c5d4303279a192b468b839f
✨ Alex ✨ <alex@dytry.ch>
2021-01-04 22:17:02 +0000

    1.1-solution: Adding two numbers together

--- lisp.js
+++ lisp.js
@@ -1,5 +1,14 @@
 function evaluateExpression(expression) {
-  return expression;
+  // if our expression is not an array, it must be an atom, so we can return it
+  // directly without any further processing
+  if (!Array.isArray(expression)) {
+    return expression;
+  }
+
+  // if our expression is an array, it must be an s-expression, so we can
+  // evaluate it!
+  const [, a, b] = expression;
+  return a + b;
 }
 
 // I'm using node's built-in common JS instead of ES modules because i'm too
