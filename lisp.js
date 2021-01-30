b1b673b43741a8268a14274abe7ce4cdd893f0f1
✨ Alex ✨ <alex@dytry.ch>
2021-01-30 15:29:22 +0000

    1.0-tests: Atoms

---
+++ lisp.js
@@ -0,0 +1,7 @@
+function evaluateExpression() {
+  // TODO: implement me
+}
+
+// I'm using node's built-in common JS instead of ES modules because i'm too
+// lazy to mess around with babel/typescript/whatever.
+module.exports = { evaluateExpression };
--- lisp.test.js
+++ lisp.test.js
@@ -1,3 +1,11 @@
-it('works', () => {
-  expect(2 + 2).toBe(4);
+const { evaluateExpression } = require('./lisp.js');
+
+describe('evaluateExpression', () => {
+  it('evaluates atoms', () => {
+    expect(evaluateExpression(1)).toBe(1);
+    expect(evaluateExpression(-101)).toBe(-101);
+    expect(evaluateExpression(123.456)).toBe(123.456);
+    expect(evaluateExpression(true)).toBe(true);
+    expect(evaluateExpression(null)).toBe(null);
+  });
 });
